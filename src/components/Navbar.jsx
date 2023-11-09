f56d3b8f521aca1d76e0e3225a0f41322dc7edbd
Przemysław Kitowski <kitowski.przemek@gmail.com>
2023-11-09 20:02:06 +0100

    Finished Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,33 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<AppBar
+			position={'static'}
+			sx={{ marginBottom: '40px' }}
+		>
+			<Toolbar
+				component={'nav'}
+				sx={{ justifyContent: 'space-between' }}
+			>
+				<Typography variant={'h6'}>AI IMAGE GENERATOR</Typography>
+				<Box
+					component='ul'
+					sx={{ display: 'flex', alignItems: 'center', gap: 3 }}
+				>
+					<Button>History</Button>
+
+					<Button disabled>Settings</Button>
+
+					<Button variant={'contained'}>Logout</Button>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	)
 }
 
 export default Navbar
