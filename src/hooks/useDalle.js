61050817c4a1826bd5b2e94d8bdec2958b745c33
Przemysław Kitowski <kitowski.przemek@gmail.com>
2023-11-25 13:59:40 +0100

    Created useDalle hook

---
+++ src/hooks/useDalle.js
@@ -0,0 +1,99 @@
+import { useReducer, useState, useEffect } from 'react'
+import { useFirestore } from './useFirestore'
+
+const initialState = {
+	isLoading: false,
+	error: false,
+	images: null,
+	success: false,
+}
+
+const dalleReducer = (state, action) => {
+	switch (action.type) {
+		case 'IS_LOADING':
+			return {
+				...state,
+				isLoading: true,
+				error: null,
+				images: null,
+				success: false,
+			}
+		case 'IMAGES_GENERATED_SUCCESSFULLY':
+			return {
+				...state,
+				isLoading: false,
+				error: null,
+				images: action.payload,
+				success: true,
+			}
+		case 'ERROR':
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+				images: null,
+				success: false,
+			}
+		default:
+			return state
+	}
+}
+
+export const useDalle = () => {
+	const [response, dispatch] = useReducer(dalleReducer, initialState)
+	const [isCancelled, setIsCancelled] = useState()
+
+	const { addDocument } = useFirestore('history')
+
+	const generateImages = async (prompt) => {
+		if (!prompt) return
+		dispatch({ type: 'IS_LOADING' })
+
+		try {
+			const response = await fetch(
+				'https://api.openai.com/v1/images/generations ',
+				{
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json',
+						Authorization: `Bearer ${import.meta.env.VITE_DALLE_API_KEY}`,
+					},
+					body: JSON.stringify({
+						prompt: `${prompt}`,
+						n: 5,
+						model: 'dall-e-2',
+						size: '512x512',
+					}),
+				}
+			)
+
+			const data = await response.json()
+			if (!isCancelled) {
+				dispatch({ type: 'IMAGES_GENERATED_SUCCESSFULLY', payload: data.data })
+			}
+
+			const imagesToAdd = data.data.map((image) => ({
+				url: image.url,
+				id: Math.floor(Math.random() * 1_000_000),
+			}))
+
+			//Adding images to firebase history
+			addDocument({
+				prompt,
+				images: imagesToAdd,
+			})
+		} catch (error) {
+			if (isCancelled) {
+				dispatch({ type: 'ERROR', payload: error.message })
+			}
+		}
+	}
+	useEffect(() => {
+		return () => {
+			setIsCancelled(true)
+		}
+	}, [])
+	return { generateImages, response }
+}
+
+export default useDalle
